71aee539031b3121e78c5559c29a5641f4d69d51
xella <xella.design@gmail.com>
2017-08-30 13:36:31 +0200

    ProductDbHelper added

---
+++ app/src/main/java/com/example/xella/inventoryapp/data/ProductDbHelper.java
@@ -0,0 +1,53 @@
+package com.example.xella.inventoryapp.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.xella.inventoryapp.data.ProductContract.ProductEntry;
+
+public class ProductDbHelper  extends SQLiteOpenHelper{
+
+    /** Name of the database file */
+    private static final String DATABASE_NAME = "inventory.db";
+
+    /**
+     * Database version. If you change the database schema, you must increment the database version.
+     */
+    private static final int DATABASE_VERSION = 1;
+
+    /**
+     * Constructs a new instance of {@link ProductDbHelper}.
+     *
+     * @param context of the app
+     */
+    public ProductDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    /**
+     * This is called when the database is created for the first time.
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // Create a String that contains the SQL statement to create the products table
+        String SQL_CREATE_ProductS_TABLE =  "CREATE TABLE " + ProductEntry.TABLE_NAME + " ("
+                + ProductEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + ProductEntry.COLUMN_PRODUCT_NAME + " TEXT NOT NULL, "
+                + ProductEntry.COLUMN_PRODUCT_PRICE + " INTEGER NOT NULL, "
+                + ProductEntry.COLUMN_PRODUCT_QUANTITY + " INTEGER NOT NULL, "
+                + ProductEntry.COLUMN_PRODUCT_SUPPLIER + " TEXT NOT NULL, "
+                + ProductEntry.COLUMN_PRODUCT_IMAGE + " BLOB NOT NULL);";
+
+        // Execute the SQL statement
+        db.execSQL(SQL_CREATE_ProductS_TABLE);
+    }
+
+    /**
+     * This is called when the database needs to be upgraded.
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // The database is still at version 1, so there's nothing to do be done here.
+    }
+}
