39d7396b4b0820ad583aabf46249f0f6449023ba
xella <xella.design@gmail.com>
2017-08-31 16:11:38 +0200

    Drawable resourses for floating action button and for "add image" button added; EditorActivity added; Layout for CatalogActivity and EditorActivity added;

---
+++ app/src/main/java/com/example/xella/inventoryapp/CatalogActivity.java
@@ -0,0 +1,26 @@
+package com.example.xella.inventoryapp;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CatalogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalog);
+
+        // Setup FAB to open EditorActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
